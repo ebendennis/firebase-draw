cfcad276e783d22978296c57464b8fa64403e1a9
ebendennis <ebendennis@gmail.com>
2015-06-18 09:52:42 -0600

    basemap

--- upload.php
+++ upload.php
@@ -28,7 +28,7 @@ if ($_FILES["photo"]["size"] > 5000000) {
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" && $imageFileType != "JPG" ) {
+&& $imageFileType != "gif" && $imageFileType != "JPG" && $imageFileType != "PNG" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
